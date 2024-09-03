ea6594812eb6e90ed0d0095543e9d7d438b8767f
DensJCN <140446584+DensJCN@users.noreply.github.com>
2024-09-03 10:00:07 -0500

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tienda/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "ecommerce";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "PEN";
+const CLIENT_ID = "AdasEQGS0f0XDPOXO6dMYEIQ3Z9OAlT-VGVyRYiGRQCcMCcRn0grqIN3E03t8oRG4THNqEx6EphrskCH";
+?>
